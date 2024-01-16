689b638f420a91e48d5ecf314f2cf2c9c317ba35
IAmMoltony <koronavirusnyj@gmail.com>
2024-01-16 14:29:20 +0600

    Fix warnings and add comments

--- source/main.c
+++ source/main.c
@@ -2,19 +2,25 @@
 
 int MainHandler(int ev, int a, int b)
 {
+    // avoid unused parameter warning
+    (void)b;
+    (void)a;
+
     switch (ev) {
         case EVT_INIT: {
-            ifont *font = OpenFont("LinerationSans", 52, 1);
-            ClearScreen();
-            SetFont(font, BLACK);
-            DrawTextRect(0, ScreenHeight() / 2 - 26, ScreenWidth(), 52, "Halo world", ALIGN_CENTER);
-            FullUpdate();
-            CloseFont(font);
+            ifont *font = OpenFont("LiberationSans", 52, 1); // open font
+            ClearScreen(); // clear screen
+            SetFont(font, BLACK); // use font with black color
+            DrawTextRect(0, ScreenHeight() / 2 - 26, ScreenWidth(), 52, "Halo world", ALIGN_CENTER); // draw text at the center of the screen
+            FullUpdate(); // update the entire screen
+            CloseFont(font); // close the font
             break;
         }
         case EVT_KEYPRESS: {
+            // The "a" parameter holds the pressed key
+            // when this event gets triggered
             if (a == KEY_OK) {
-                CloseApp();
+                CloseApp(); // close app
             }
             break;
         }
@@ -25,6 +31,9 @@ int MainHandler(int ev, int a, int b)
 
 int main(int argc, char **argv)
 {
+    (void)argc;
+    (void)argv;
+
     InkViewMain(MainHandler);
     return 0;
 }
